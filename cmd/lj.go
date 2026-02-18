c669f22b7d82f5f5dd8e9d0258544364d13cbfc7
xnacly <47723417+xNaCly@users.noreply.github.com>
2026-02-18 14:26:15 +0100

    cmd/lj: rework the cli with options and flags

--- cmd/lj.go
+++ cmd/lj.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"encoding/json"
+	"flag"
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
+	"runtime/debug"
+	"runtime/pprof"
 
 	"github.com/xnacly/libjson"
 )
@@ -16,17 +21,59 @@ func Must[T any](t T, err error) T {
 }
 
 func main() {
-	args := os.Args
+	noGc := flag.Bool("nogc", false, "disable the go garbage collector")
+	useLibjson := flag.Bool("libjson", true, "use libjson, if false use encoding/json")
+	usePprof := flag.Bool("pprof", false, "use pprof cpu tracing")
+	query := flag.String("q", ".", "query the parsed json")
+	silent := flag.Bool("s", false, "no stdoutput")
+	flag.Parse()
+
+	if *noGc {
+		debug.SetGCPercent(-1)
+	}
+
+	args := flag.Args()
+
+	var filePath string
 	var file *os.File
 	if info, err := os.Stdin.Stat(); err != nil || info.Mode()&os.ModeCharDevice != 0 { // we are in a pipe
-		if len(args) == 1 {
-			log.Fatalln("Wanted a file as first argument, got nothing, exiting")
+		if len(args) == 0 {
+			log.Fatalln("Wanted a file as an argument, got nothing, exiting")
 		}
-		file = Must(os.Open(args[1]))
+		filePath = args[0]
+		file = Must(os.Open(filePath))
 	} else {
 		file = os.Stdin
+		filePath = "stdin"
 	}
-	query := os.Args[len(os.Args)-1]
-	json := Must(libjson.NewReader(file))
-	fmt.Printf("%+#v\n", Must(libjson.Get[any](&json, query)))
+
+	if *usePprof {
+		f, err := os.Create(filepath.Base(filePath) + ".pprof")
+		if err != nil {
+			panic(err)
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
+
+	if *useLibjson {
+		out := Must(libjson.NewReader(file))
+		if !*silent {
+			fmt.Printf("%+#v\n", Must(libjson.Get[any](&out, *query)))
+		}
+	} else {
+		if *query != "." {
+			panic("With -libjson=false, there is no support for querying the json")
+		}
+
+		decoder := json.NewDecoder(file)
+		var a any
+		if err := decoder.Decode(&a); err != nil {
+			panic(err)
+		}
+
+		if !*silent {
+			fmt.Printf("%+#v\n", a)
+		}
+	}
 }
