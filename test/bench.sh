7410c209396338266a5f1562fdb85c595a1b63dd
xnacly <47723417+xNaCly@users.noreply.github.com>
2026-02-18 14:26:42 +0100

    test: replace test binary with cmd/lj

--- test/bench.sh
+++ test/bench.sh
@@ -4,8 +4,8 @@ python3 gen.py
 
 echo "building executable"
 rm ./test
-go build ./test.go
+go build -o ./test ../cmd/lj.go
 
-hyperfine "./test ./1MB.json" "./test -libjson=false ./1MB.json"
-hyperfine "./test ./5MB.json" "./test -libjson=false ./5MB.json"
-hyperfine "./test ./10MB.json" "./test -libjson=false ./10MB.json"
+hyperfine "./test -s ./1MB.json" "./test -s -libjson=false ./1MB.json"
+hyperfine "./test -s ./5MB.json" "./test -s -libjson=false ./5MB.json"
+hyperfine "./test -s ./10MB.json" "./test -s -libjson=false ./10MB.json"
